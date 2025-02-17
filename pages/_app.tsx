1b20acbfadbebe3ecb6c73f847a0ea17b0157e9a
HyeonjiChae <chawl@naver.com>
2025-02-17 16:40:57 +0900

    lint 설치 후 package.json에 추가 / Lint 실행하여 import 자동 수정
    lint 설치 후 package.json에 추가 / Lint 실행하여 import 자동 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
+// ✅ 스타일 관련 import (CSS 관련 파일은 가장 마지막에 배치)
+import 'katex/dist/katex.min.css' // 수식 렌더링
+import 'prismjs/themes/prism-coy.css' // 코드 하이라이팅
+import 'react-notion-x/src/styles.css' // Notion 스타일
+import 'styles/global.css' // 전역 스타일
+import 'styles/notion.css' // Notion 스타일 오버라이드
+import 'styles/prism-theme.css' // Prism 테마
+import 'prismjs/themes/prism-okaidia.css'
+
 import type { AppProps } from 'next/app'
-import * as React from 'react'
-import { useRouter } from 'next/router'
 import * as Fathom from 'fathom-client'
+// ✅ 폰트 로컬 임포트
+import localFont from 'next/font/local'
+import { useRouter } from 'next/router'
 import posthog from 'posthog-js'
+import * as React from 'react'
 
 import { bootstrap } from '@/lib/bootstrap-client'
 import {
@@ -13,8 +24,6 @@ import {
   posthogId
 } from '@/lib/config'
 
-// ✅ 폰트 로컬 임포트
-import localFont from 'next/font/local'
 const pretendard = localFont({
   src: '../public/fonts/PretendardGOVVariable.woff2',
   display: 'swap',
@@ -24,16 +33,7 @@ const wanted = localFont({
   src: '../public/fonts/WantedSansVariable.woff2',
   display: 'swap',
   variable: '--font-wanted'
-})
-
-// ✅ 스타일 관련 import (CSS 관련 파일은 가장 마지막에 배치)
-import 'katex/dist/katex.min.css' // 수식 렌더링
-import 'prismjs/themes/prism-coy.css' // 코드 하이라이팅
-import 'react-notion-x/src/styles.css' // Notion 스타일
-import 'styles/global.css' // 전역 스타일
-import 'styles/notion.css' // Notion 스타일 오버라이드
-import 'styles/prism-theme.css' // Prism 테마
-import 'prismjs/themes/prism-okaidia.css' // 다크 모드용 (필요하면 활성화)
+}) // 다크 모드용 (필요하면 활성화)
 
 if (!isServer) {
   bootstrap()
